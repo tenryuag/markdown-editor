64877ab8af48059aa0f14d32caf5e5e605e6e216
tenryuag <tenryuabe@gmail.com>
2024-09-29 17:34:28 +0900

    react install

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
