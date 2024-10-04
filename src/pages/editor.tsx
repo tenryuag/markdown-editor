74dd36cb90010d6e0314e1d2dd73f62aa052690e
tenryuag <tenryuabe@gmail.com>
2024-10-04 09:15:38 +0900

    add history and historyPages

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -2,24 +2,33 @@ import * as React from 'react'
 import styled from 'styled-components'
 import { useStateWithStorage } from '../hooks/use_state_with_storage'
 import * as ReactMarkdown from 'react-markdown'
+import { putMemo } from '../indexeddb/memos'
+import { Button } from '../components/button'
+import { SaveModal } from '../components/save_modal'
+import { Link } from 'react-router-dom'
+import { Header } from '../components/header'
 
-const Header = styled.header`
-  font-size: 1.5rem;
-  height: 2rem;
-  left: 0;
-  line-height: 2rem;
-  padding: 0.5rem 1rem;
-  position: fixed;
-  right: 0;
-  top: 0;
-`
+const { useState } = React
 
 const Wrapper = styled.div`
-  bottom: 0;
+    bottom: 0;
   left: 0;
   position: fixed;
   right: 0;
   top: 3rem;
+`
+
+const HeaderControl = styled.div`
+    height: 2rem;
+    display: flex;
+    align-content: center;
+  `
+
+const HeaderArea = styled.div`
+  position: fixed;
+  right: 0;
+  top: 0;
+  left: 0;
 `
 
 const TextArea = styled.textarea`
@@ -45,16 +54,28 @@ const Preview = styled.div`
   width: 50vw;
 `
 
-const StorageKey = 'pages/editor:text'
+interface Props {
+  text: string
+  setText: (text: string) => void
+}
 
-export const Editor: React.FC = () => {
-  const [text, setText] = useStateWithStorage('', StorageKey)
+export const Editor: React.FC<Props> = (props) => {
+  const { text, setText } = props
+
+  const [showModal, setShowModal] = useState(false)
 
   return (
     <>
-      <Header>
-        Markdown Editor
-      </Header>
+      <HeaderArea>
+        <Header title="Markdown Editor">
+          <Button onClick={() => setShowModal(true)}>
+            保存する
+          </Button>
+          <Link to="/history">
+            履歴を見る
+          </Link>
+        </Header>
+      </HeaderArea>
       <Wrapper>
         <TextArea
           onChange={(event) => setText(event.target.value)}
@@ -64,6 +85,15 @@ export const Editor: React.FC = () => {
           <ReactMarkdown>{text}</ReactMarkdown>
         </Preview>
       </Wrapper>
+      {showModal && (
+        <SaveModal
+          onSave={(title: string): void => {
+            putMemo(title, text)
+            setShowModal(false)
+          }}
+          onCancel={() => setShowModal(false)}
+        />
+      )}
     </>
   )
 }
